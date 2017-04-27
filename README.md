0add3dff20e8b8769655d1a203ddf3071980d1fc
PharaohKJ <kato@phalanxware.com>
2017-04-27 12:12:47 +0900

    1st commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# これは何か
+
+CfK Civic Hack Night 向け GitHub issues 自動作成ツールです。
+
+.envファイルにアクセストークンなどを書いて `ruby main.rb task.yml` で実行。
---
+++ main.rb
@@ -0,0 +1,25 @@
+# coding: utf-8
+require 'octokit'
+require 'dotenv'
+require 'yaml'
+
+Dotenv.load
+
+exit if ARGV.empty?
+
+yml = YAML.load_file(ARGV[0])
+
+client = Octokit::Client.new(
+  login:    ENV['GITHUB_USER'],
+  password: ENV['GITHUB_TOKEN']
+)
+
+repository = ENV[TEST_REPO] || yml['repository']
+eventname  = yml['event']
+yml['tasks'].each do |t|
+  p client.create_issue(
+    repository,
+    "#{eventname} - #{t['title']}",
+    t['body']
+  )
+end
